dcc0fe85042972a8979e87936ca8d5cb2eb80f27
chenx <bufubaoni@163.com>
2017-01-21 16:25:35 +0800

    delete some html code

--- zhihu_object.py
+++ zhihu_object.py
@@ -12,36 +12,10 @@ class ZhiHuSpider(object):
         self._url = url
         self._headers = headers
         self._session = Session()
-        self._first_html = self.get_first_html()
-        self._current_page = 1
-        self._pages = self.get_pages()
+
         self._api_url = api_url
         self._api_content = self.get_api_object()
 
-
-    def get_first_html(self):
-        self._html = self._session.get(headers=self._headers, url=self._url).text
-        return self._html
-
-    def get_pages(self):
-        _get_pages = pq(self._html)("div.Pagination>button")
-        if _get_pages:
-            return int(_get_pages[-2].text)
-        else:
-            return 1
-
-    def get_next_page_url(self):
-        return self._url + "?page={page}".format(page=self._current_page + 1)
-
-    def get_next_html(self):
-        if self._current_page < self._pages:
-            self._html = self._session.get(headers=self._headers, url=self.get_next_page_url()).text
-            return self._html
-        else:
-            return None
-
-    def get_html(self):
-        return self._html
 
     def get_api_object(self):
         return self._session.get(headers=self._headers, url=self._api_url).text
