2db275ea99729cd3d16e158313a3d4fa3d0a37c4
chenx <bufubaoni@163.com>
2017-01-22 20:43:49 +0800

    following

--- following.py
+++ following.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Alex on 2017/1/20
+from zhihu_object import ZhiHuSpider
+
+
+class Following(ZhiHuSpider):
+    def __init__(self,name):
+        # chen-er-bai-18
+        self._url = ("https://www.zhihu.com/api/v4/members/{name}/followees?"
+                     "include=data[*].answer_count,"
+                     "articles_count,"
+                     "follower_count,"
+                     "is_followed,"
+                     "is_following,"
+                     "badge[?(type=best_answerer)].topics&offset={offset}&limit=20")
+        super(Following, self).__init__(self._url, name)
+
+if __name__ == "__main__":
+    fling = Following(name="chen-er-bai-18")
+
+    for i in fling.get_api_object():
+        print i["name"]
