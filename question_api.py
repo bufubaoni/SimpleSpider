d557be508e18cc71af64069a6c7b27ee41246309
chenx <bufubaoni@163.com>
2017-02-27 16:55:02 +0800

    they change api so update

---
+++ question_api.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# zhihu.com update there api
+from zhihu_object import ZhiHuSpider
+
+
+class Question(ZhiHuSpider):
+    def __init__(self):
+        pass
+
